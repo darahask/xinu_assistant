9985c8293bdf54ce6234bf040d67b5a48b30a428
kirtik_jangale <kirtik.jangale2000@gmail.com>
2021-05-02 15:27:02 +0530

    removed unnecessary code

--- assistant/shell/xsh_player.c
+++ assistant/shell/xsh_player.c
@@ -1,51 +1,12 @@
 /* xsh_player.c - xsh_player */
-
 #include <xinu.h>
-
-#include <stdio.h>
-
-#include <string.h>
-
-#include <stdlib.h>
- // #include <time.h>
 
 /*------------------------------------------------------------------------
  * xsh_player - shell command to manage todo commands(create, delete, print, etc.)
  *------------------------------------------------------------------------
  */
 
-// typedef struct Queue queue;
-
-// struct Queue {
-
-// 	char * songName;
-// 	queue * next;
-// };
-
-// queue * node1, * node2, * start;
-
-
-// start->songName = "Song ABC";
-// start->next = node1;
-
-// node1->songName = "Song PQR";
-// node->next = node2;
-
-// node1->songName = "Song XYZ";
-// node1->next = NULL;
-
-char queue[3][20] = {"Song ABC","Song PQR","Song XYZ"};
-int32 qsize=3;
-
-
 shellcmd xsh_player(int nargs, char * args[]) {
-	int32 i; /* index into proctabl		*/
-	int32 nextch; /* character read from file	*/
-	did32 descr; /* descriptor for a file	*/
-	char * argptr; /* pointer to next arg string	*/
-	// FILE    *playerptr;       /* Todo file db pointer */
-
-
 
     if (nargs == 2 && strncmp(args[1], "stop", 4) == 0) {
 		if(musicprocid){
@@ -102,37 +63,6 @@ shellcmd xsh_player(int nargs, char * args[]) {
 		}
 		
 	}
-
-
-	if (nargs == 2 && strncmp(args[1], "queue", 5) == 0) {
-		kprintf("\nSongs in Queue:\n\n");
-
-		// queue * tmp;
-		// tmp = start;
-		// if (start == NULL) {
-		// 	kprintf("No songs added to queue\n\n");
-		// }
-
-		// int cnt = 0;
-		// while (tmp != NULL) {
-		// 	kprintf("%d. %s\n", cnt, tmp - > songName);
-		// }
-
-		for(i=0;i<qsize;i++){
-			kprintf("%d. %s\n",i+1,queue[i]);
-		}
-	}
-  
-	if (nargs == 3){
-		if(strncmp(args[1],"remove",6) == 0){
-			char * song_id = args[2];
-			//Remove the song file 
-			printf("Removed song wth id %s\n", song_id);
-		}
-
-	}
-
-	
 
 	return 0;
 }
